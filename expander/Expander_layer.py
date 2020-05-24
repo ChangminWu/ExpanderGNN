5bc31826bbaa42b47812319066237fbf0a4c64ff
Changmin <47852417+ChangminWu@users.noreply.github.com>
2020-05-24 23:43:28 +0200

    typo

--- expander/Expander_layer.py
+++ expander/Expander_layer.py
@@ -29,7 +29,7 @@ class ExpanderLinear(torch.autograd.Function):
             grad_weight.mul_(ctx.mask)
         if bias is not None and ctx.needs_input_grad[2]:
             grad_bias = grad_output.sum(0)
-        return grad_input, grad_weight, grad_bias
+        return grad_input, grad_weight, grad_bias, None
 
 
 class ExpanderLinearLayer(nn.Module):
