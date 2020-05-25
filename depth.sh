e3d270956cfc260b35da7fadb5f076908a96a8f1
ChangminWu <changmin.wu@polytechnique.edu>
2020-05-25 03:23:46 +0200

    typo

--- depth.sh
+++ depth.sh
@@ -12,27 +12,27 @@ do
     do
       if [[ $k == *"SimpleGCN"* ]];
       then
-        config_file=configs/graph_classification_SimpleGCN_${i}.json
+          config_file=configs/graph_classification_SimpleGCN_${i}.json
       elif [[ $k == *"GatedGCN"* ]];
       then
-        config_file=configs/graph_classification_GatedGCN_${i}.json
+          config_file=configs/graph_classification_GatedGCN_${i}.json
       elif [[ $k == *"GCN"* ]];
       then
-        config_file=configs/graph_classification_GCN_${i}.json
+          config_file=configs/graph_classification_GCN_${i}.json
       elif [[ $k == *"GIN"* ]];
       then
-        config_file=configs/graph_classification_GIN_${i}.json
+          config_file=configs/graph_classification_GIN_${i}.json
       elif [[ $k == *"MLP"* ]];
       then
-        config_file=configs/graph_classification_MLP_${i}.json
+          config_file=configs/graph_classification_MLP_${i}.json
       elif [[ $k == *"Sage"* ]];
       then
-        config_file=configs/graph_classification_GraphSage_${i}.json
+          config_file=configs/graph_classification_GraphSage_${i}.json
       else
-        echo "wrong model name"
-        exit
+          echo "wrong model name"
+          exit
       fi
-      python $code --dataset ${i} --experiment "varing-depth-normal" --model ${k} --sparsities 0.1 --config ${config_file} --L ${j}
+      python $script --dataset ${i} --experiment "varing-depth-normal" --model ${k} --sparsity 0.1 --config ${config_file} --L ${j}
       sleep 15
     done
   done
@@ -44,57 +44,57 @@ do
   do
     if [[ $k == *"SimpleGCN"* ]];
     then
-      config_file=configs/graph_classification_SimpleGCN_${i}.json
+        config_file=configs/graph_classification_SimpleGCN_${i}.json
     elif [[ $k == *"GatedGCN"* ]];
     then
-      config_file=configs/graph_classification_GatedGCN_${i}.json
+        config_file=configs/graph_classification_GatedGCN_${i}.json
     elif [[ $k == *"GCN"* ]];
     then
-      config_file=configs/graph_classification_GCN_${i}.json
+        config_file=configs/graph_classification_GCN_${i}.json
     elif [[ $k == *"GIN"* ]];
     then
-      config_file=configs/graph_classification_GIN_${i}.json
+        config_file=configs/graph_classification_GIN_${i}.json
     elif [[ $k == *"MLP"* ]];
     then
-      config_file=configs/graph_classification_MLP_${i}.json
+        config_file=configs/graph_classification_MLP_${i}.json
     elif [[ $k == *"Sage"* ]];
     then
-      config_file=configs/graph_classification_GraphSage_${i}.json
+        config_file=configs/graph_classification_GraphSage_${i}.json
     else
-      echo "wrong model name"
-      exit
+        echo "wrong model name"
+        exit
     fi
-    python $code --dataset ${i} --experiment "varing-depth-expander-l-2-mlp-1" --model ${k} --sparsities 0.1 --config ${config_file} --L 2 --n_mlp 1
+    python $script --dataset ${i} --experiment "varing-depth-expander-l-2-mlp-1" --model ${k} --sparsity 0.1 --config ${config_file} --L 2 --n_mlp 1
     sleep 10
-    python $code --dataset ${i} --experiment "varing-depth-expander-l-4-mlp-1" --model ${k} --sparsities 0.1 --config ${config_file} --L 4 --n_mlp 1
+    python $script --dataset ${i} --experiment "varing-depth-expander-l-4-mlp-1" --model ${k} --sparsity 0.1 --config ${config_file} --L 4 --n_mlp 1
     sleep 10
-    python $code --dataset ${i} --experiment "varing-depth-expander-l-2-mlp-2" --model ${k} --sparsities 0.1 --config ${config_file} --L 2 --n_mlp 2
+    python $script --dataset ${i} --experiment "varing-depth-expander-l-2-mlp-2" --model ${k} --sparsity 0.1 --config ${config_file} --L 2 --n_mlp 2
     sleep 10
-    python $code --dataset ${i} --experiment "varing-depth-expander-l-6-mlp-1" --model ${k} --sparsities 0.1 --config ${config_file} --L 6 --n_mlp 1
+    python $script --dataset ${i} --experiment "varing-depth-expander-l-6-mlp-1" --model ${k} --sparsity 0.1 --config ${config_file} --L 6 --n_mlp 1
     sleep 10
-    python $code --dataset ${i} --experiment "varing-depth-expander-l-3-mlp-2" --model ${k} --sparsities 0.1 --config ${config_file} --L 3 --n_mlp 2
+    python $script --dataset ${i} --experiment "varing-depth-expander-l-3-mlp-2" --model ${k} --sparsity 0.1 --config ${config_file} --L 3 --n_mlp 2
     sleep 10
-    python $code --dataset ${i} --experiment "varing-depth-expander-l-2-mlp-3" --model ${k} --sparsities 0.1 --config ${config_file} --L 2 --n_mlp 3
+    python $script --dataset ${i} --experiment "varing-depth-expander-l-2-mlp-3" --model ${k} --sparsity 0.1 --config ${config_file} --L 2 --n_mlp 3
     sleep 10
-    python $code --dataset ${i} --experiment "varing-depth-expander-l-8-mlp-1" --model ${k} --sparsities 0.1 --config ${config_file} --L 8 --n_mlp 1
+    python $script --dataset ${i} --experiment "varing-depth-expander-l-8-mlp-1" --model ${k} --sparsity 0.1 --config ${config_file} --L 8 --n_mlp 1
     sleep 10
-    python $code --dataset ${i} --experiment "varing-depth-expander-l-4-mlp-2" --model ${k} --sparsities 0.1 --config ${config_file} --L 4 --n_mlp 2
+    python $script --dataset ${i} --experiment "varing-depth-expander-l-4-mlp-2" --model ${k} --sparsity 0.1 --config ${config_file} --L 4 --n_mlp 2
     sleep 10
-    python $code --dataset ${i} --experiment "varing-depth-expander-l-2-mlp-4" --model ${k} --sparsities 0.1 --config ${config_file} --L 2 --n_mlp 4
+    python $script --dataset ${i} --experiment "varing-depth-expander-l-2-mlp-4" --model ${k} --sparsity 0.1 --config ${config_file} --L 2 --n_mlp 4
     sleep 10
-    python $code --dataset ${i} --experiment "varing-depth-expander-l-12-mlp-1" --model ${k} --sparsities 0.1 --config ${config_file} --L 12 --n_mlp 1
+    python $script --dataset ${i} --experiment "varing-depth-expander-l-12-mlp-1" --model ${k} --sparsity 0.1 --config ${config_file} --L 12 --n_mlp 1
     sleep 10
-    python $code --dataset ${i} --experiment "varing-depth-expander-l-6-mlp-2" --model ${k} --sparsities 0.1 --config ${config_file} --L 6 --n_mlp 2
+    python $script --dataset ${i} --experiment "varing-depth-expander-l-6-mlp-2" --model ${k} --sparsity 0.1 --config ${config_file} --L 6 --n_mlp 2
     sleep 10
-    python $code --dataset ${i} --experiment "varing-depth-expander-l-4-mlp-3" --model ${k} --sparsities 0.1 --config ${config_file} --L 4 --n_mlp 3
+    python $script --dataset ${i} --experiment "varing-depth-expander-l-4-mlp-3" --model ${k} --sparsity 0.1 --config ${config_file} --L 4 --n_mlp 3
     sleep 10
-    python $code --dataset ${i} --experiment "varing-depth-expander-l-3-mlp-4" --model ${k} --sparsities 0.1 --config ${config_file} --L 3 --n_mlp 4
+    python $script --dataset ${i} --experiment "varing-depth-expander-l-3-mlp-4" --model ${k} --sparsity 0.1 --config ${config_file} --L 3 --n_mlp 4
     sleep 10
-    python $code --dataset ${i} --experiment "varing-depth-expander-l-16-mlp-1" --model ${k} --sparsities 0.1 --config ${config_file} --L 16 --n_mlp 1
+    python $script --dataset ${i} --experiment "varing-depth-expander-l-16-mlp-1" --model ${k} --sparsity 0.1 --config ${config_file} --L 16 --n_mlp 1
     sleep 10
-    python $code --dataset ${i} --experiment "varing-depth-expander-l-8-mlp-2" --model ${k} --sparsities 0.1 --config ${config_file} --L 8 --n_mlp 2
+    python $script --dataset ${i} --experiment "varing-depth-expander-l-8-mlp-2" --model ${k} --sparsity 0.1 --config ${config_file} --L 8 --n_mlp 2
     sleep 10
-    python $code --dataset ${i} --experiment "varing-depth-expander-l-4-mlp-4" --model ${k} --sparsities 0.1 --config ${config_file} --L 4 --n_mlp 4
+    python $script --dataset ${i} --experiment "varing-depth-expander-l-4-mlp-4" --model ${k} --sparsity 0.1 --config ${config_file} --L 4 --n_mlp 4
     sleep 10
   done
 done
--- main.sh
+++ main.sh
@@ -9,35 +9,35 @@ do
   do
     if [[ $j == 0.0 ]];
     then
-      config_file=configs/graph_classification_SimpleGCN_${i}.json
-      python $code --dataset ${i} --experiment "zero-sparsity-sgcn" --model "SimpleGCN" --sparsities ${j} --config ${config_file} --n_mlp 1
-      sleep 15
+        config_file=configs/graph_classification_SimpleGCN_${i}.json
+        python $script --dataset ${i} --experiment "zero-sparsity-sgcn" --model "SimpleGCN" --sparsity ${j} --config ${config_file} --n_mlp 1
+        sleep 15
     else
       for k in "${models[@]}"
       do
         if [[ $k == *"SimpleGCN"* ]];
         then
-          config_file=configs/graph_classification_SimpleGCN_${i}.json
+            config_file=configs/graph_classification_SimpleGCN_${i}.json
         elif [[ $k == *"GatedGCN"* ]];
         then
-          config_file=configs/graph_classification_GatedGCN_${i}.json
+            config_file=configs/graph_classification_GatedGCN_${i}.json
         elif [[ $k == *"GCN"* ]];
         then
-          config_file=configs/graph_classification_GCN_${i}.json
+            config_file=configs/graph_classification_GCN_${i}.json
         elif [[ $k == *"GIN"* ]];
         then
-          config_file=configs/graph_classification_GIN_${i}.json
+            config_file=configs/graph_classification_GIN_${i}.json
         elif [[ $k == *"MLP"* ]];
         then
-          config_file=configs/graph_classification_MLP_${i}.json
-	      elif [[ $k == *"Sage"* ]];
+            config_file=configs/graph_classification_MLP_${i}.json
+        elif [[ $k == *"Sage"* ]];
         then
-          config_file=configs/graph_classification_GraphSage_${i}.json
+            config_file=configs/graph_classification_GraphSage_${i}.json
         else
-          echo "wrong model name"
-          exit
+            echo "wrong model name"
+            exit
         fi
-        python $code --dataset ${i} --experiment "varing-sparsity" --model ${k} --sparsities ${j} --config ${config_file} --n_mlp 1
+        python $script --dataset ${i} --experiment "varing-sparsity" --model ${k} --sparsity ${j} --config ${config_file} --n_mlp 1
         sleep 15
       done
     fi
