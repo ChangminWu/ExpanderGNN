2eb3e680a90359d44f98010f2c359278bdaa8864
Changmin <47852417+ChangminWu@users.noreply.github.com>
2020-07-11 00:43:38 +0200

    remove old files

--- clean.sh
+++
@@ -1,8 +0,0 @@
-rm -r baselines/layers/__pycache__
-rm -r baselines/nets/__pycache__
-rm -r expander/__pycache__
-rm -r models/__pycache__
-rm -r data/__pycache__
-rm -r __pycache__
-rm -r configs/.ipynb_checkpoints
-rm -r postprocess/.ipynb_checkpoints
