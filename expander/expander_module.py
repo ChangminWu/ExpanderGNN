24d27cbec15f12f70d5e87ccede9f58cfef7d7f7
changmin <changmin@pop-os.localdomain>
2020-08-07 18:58:55 +0200

    add sampler

--- expander/expander_module.py
+++ expander/expander_module.py
@@ -1,16 +1,13 @@
 import torch 
 import torch.nn as nn
+from .expander_layer import ExpanderLinearFunction
+from .samplers import sampler
 
 
 class ExpanderLinear(nn.Module):
     def __init__(self, indim, outdim, density=None, bias=True, sampler="regular"):
         super(ExpanderLinear, self).__init__()
-        self.indim, self.outdim, self.density = indim, outdim, density
-        
-        if self.density is not None:
-            self.n_params = min(self.indim, self.outdim) * int(max(self.indim, self.outdim) * self.density)
-        else:
-            self.n_params = self.indim * self.outdim
+        self.indim, self.outdim, self.density, self.sampler = indim, outdim, density, sampler
 
         self.weight = nn.Parameter(data=torch.Tensor(self.outdim, self.indim))
         
@@ -24,6 +21,20 @@ class ExpanderLinear(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
+        nn.init.xavier_uniform_(self.weight)
+        # nn.init.kaiming_normal_(self.weight, mode="fan_ins")
+        if self.bias is not None:
+            nn.init.zeros_(self.bias)
+
+    def forward(self, _input):
+        return ExpanderLinearFunction.apply(_input, self.weight, self.mask, self.bias)
+
+    def generate_mask(self, init=None):
+        if init is None:
+            self.mask, self.n_params = sampler(self.outdim, self.indim, self.density, method=self.sampler)
+        else:
+            self.n_params == torch.sum(init)
+            self.mask = init
         
     
         
