13fa351c740c3c2e13cf6ecb5790eeec046bb5dc
Changmin <47852417+ChangminWu@users.noreply.github.com>
2020-08-27 17:35:05 +0200

    change test file

--- test_enzymes.sh
+++ test_enzymes.sh
@@ -12,13 +12,13 @@ do
     config_file=configs/tu_graph_classification_${j}_${i}_100k.json
     for d in "${densities[@]}"
     do
-      python $script --dataset ${i} --out_dir ${savedir} --experiment "expander-density-${d}" --model ${j} --density ${d} --linear_type "expander" --config ${config_file}
+      python $script --dataset ${i} --out_dir ${savedir} --experiment "expander-density-${d}" --model ${j} --density ${d} --linear_type "expander" --config ${config_file} --epochs 3
     done
     for a in "${actives[@]}"
     do
-      python $script --dataset ${i} --out_dir ${savedir} --experiment "activations-${a}" --model "Activation${j}" --activation ${a} --config ${config_file}
+      python $script --dataset ${i} --out_dir ${savedir} --experiment "activations-${a}" --model "Activation${j}" --activation ${a} --config ${config_file} --epochs 3
     done
-    python $script --dataset ${i} --out_dir ${savedir} --experiment "simple" --model "Simple${j}" --config ${config_file}
-    python $script --dataset ${i} --out_dir ${savedir} --experiment "regular" --model ${j} --density 1.0 --config ${config_file}
+    python $script --dataset ${i} --out_dir ${savedir} --experiment "simple" --model "Simple${j}" --config ${config_file} --epochs 3
+    python $script --dataset ${i} --out_dir ${savedir} --experiment "regular" --model ${j} --density 1.0 --config ${config_file} --epochs 3
   done
 done
