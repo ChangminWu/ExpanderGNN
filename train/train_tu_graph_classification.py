f2c84c06efb39c9724b38ab38a5c855acba41f67
Changmin <47852417+ChangminWu@users.noreply.github.com>
2020-08-28 07:48:42 +0200

    dgl graph to device error

--- train/train_tu_graph_classification.py
+++ train/train_tu_graph_classification.py
@@ -11,6 +11,7 @@ def train_epoch_sparse(model, optimizer, device, data_loader, epoch,
     epoch_train_acc = 0
     nb_data = 0
     for iter, (batch_graphs, batch_labels) in enumerate(data_loader):
+        batch_graphs = batch_graphs.to(device)
         batch_x = batch_graphs.ndata['feat'].to(device)  # num x feat
         batch_e = batch_graphs.edata['feat'].to(device)
         batch_labels = batch_labels.to(device)
