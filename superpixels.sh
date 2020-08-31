441ae1fe06638da57316a1d5715444b87bd7faaa
Changmin <47852417+ChangminWu@users.noreply.github.com>
2020-08-31 18:06:44 +0200

    superpixel update

---
+++ superpixels.sh
@@ -0,0 +1,24 @@
+script=main_superpixel_graph_classification.py
+datasets=("MNIST" "CIFAR10")
+actives=('relu' 'prelu' 'linear' 'brelu' 'brelu-intercept' 'conv' 'rrelu' 'elu' 'sigmoid' 'tanh' 'lelu' 'softplus')
+models=('GCN' 'GIN' 'MLP' 'GraphSage' 'GatedGCN' 'GAT' 'PNA')
+densities=( 0.1 0.5 0.9 )
+savedir="results/superpixel-runs/"
+
+for i in "${datasets[@]}"
+do
+  for j in "${models[@]}"
+  do
+    config_file=configs/superpixel_graph_classification/${j}_${i}_100k.json
+    for d in "${densities[@]}"
+    do
+      python $script --dataset ${i} --out_dir ${savedir} --experiment "expander-density-${d}" --model ${j} --density ${d} --linear_type "expander" --config ${config_file} --mlp_layers 1
+    done
+    for a in "${actives[@]}"
+    do
+      python $script --dataset ${i} --out_dir ${savedir} --experiment "activations-${a}" --model "Activation${j}" --activation ${a} --config ${config_file} --mlp_layers 1
+    done
+    python $script --dataset ${i} --out_dir ${savedir} --experiment "simple" --model "Simple${j}" --config ${config_file} --mlp_layers 1
+    python $script --dataset ${i} --out_dir ${savedir} --experiment "regular" --model ${j} --linear_type "regular" --density 1.0 --config ${config_file} --mlp_layers 1
+  done
+done
