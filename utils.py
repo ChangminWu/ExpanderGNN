84cbc6d336e25a49a93cae3a592606dde85bcf02
Changmin <47852417+ChangminWu@users.noreply.github.com>
2020-09-01 05:00:55 +0200

    update

--- utils.py
+++ utils.py
@@ -107,7 +107,7 @@ def expander_weights_writer(net, saved_expander, saved_layers=None,
             mask = weight.copy()
             mask[mask != 0] = 1
             print(net)
-            print("mask ", mask.sum(), " size", mask.size())
+            print("mask ", mask.sum(), " size ", mask.shape)
             print("expander ", saved_expander[label].cpu().detach().numpy().sum(), " size", saved_expander[label].cpu().detach().numpy().shape)
             assert (mask == saved_expander[label].cpu().detach().numpy()).all()
             # with open(curr_path + label + ".pickle") as f:
