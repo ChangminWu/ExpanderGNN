ec9429b862299041bdf47731c4516f76a02507ae
Changmin <47852417+ChangminWu@users.noreply.github.com>
2020-09-17 05:10:15 +0200

    new tests

--- test_enzymes.sh
+++ test_enzymes.sh
@@ -1,10 +1,11 @@
 use_gpu=True
 script=main_tu_graph_classification.py
-datasets=("ENZYMES" "DD" "PROTEINS_full" "REDDIT-BINARY" "IMDB-BINARY")
-actives=('relu' 'prelu' 'linear' 'brelu' 'brelu-intercept' 'rrelu' 'softplus')
+datasets=("DD" "PROTEINS_full" "REDDIT-BINARY" "IMDB-BINARY")
+actives=("linear")
+# 'relu' 'prelu' 'linear' 'brelu' 'brelu-intercept' 'rrelu' 'softplus'
 models=('GCN' 'GIN' 'MLP' 'GraphSage' 'GatedGCN' 'GAT' 'PNA')
-densities=( 0.1 0.5 0.9 )
-savedir="results/test/"
+densities=( 0.1 )
+savedir="results/test-new/"
 
 for i in "${datasets[@]}"
 do
@@ -24,7 +25,7 @@ do
     do
       python $script --dataset ${i} --out_dir ${savedir} --experiment "activations-${a}" --model "Activation${j}" --activation ${a} --config ${config_file} --epochs 2 --mlp_layers 1 --use_gpu $use_gpu --num_split 3
     done
-    python $script --dataset ${i} --out_dir ${savedir} --experiment "simple" --model "Simple${j}" --config ${config_file} --epochs 2 --mlp_layers 1 --use_gpu $use_gpu --num_split 3
-    python $script --dataset ${i} --out_dir ${savedir} --experiment "regular" --model ${j} --density 1.0 --config ${config_file} --epochs 2 --mlp_layers 1 --use_gpu $use_gpu --num_split 3
+    # python $script --dataset ${i} --out_dir ${savedir} --experiment "simple" --model "Simple${j}" --config ${config_file} --epochs 2 --mlp_layers 1 --use_gpu $use_gpu --num_split 3
+    # python $script --dataset ${i} --out_dir ${savedir} --experiment "regular" --model ${j} --density 1.0 --config ${config_file} --epochs 2 --mlp_layers 1 --use_gpu $use_gpu --num_split 3
   done
 done
