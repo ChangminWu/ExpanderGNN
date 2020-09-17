2ca310cf868398b4c0a7fe3f76081dd70cf70e2b
Changmin <47852417+ChangminWu@users.noreply.github.com>
2020-09-17 18:17:15 +0200

    citation change

--- train/train_citation_node_classification.py
+++ train/train_citation_node_classification.py
@@ -8,16 +8,14 @@ def train_epoch_sparse(model, optimizer, device, graph, epoch,
                        nfeat, efeat, train_mask, labels,
                        writer=None):
     model.train()
-    epoch_loss = 0
-    epoch_train_acc = 0
 
     logits = model(graph, nfeat, efeat)
     loss = model.loss(logits[train_mask], labels[train_mask])
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-    epoch_loss += loss.detach().item()
-    epoch_train_acc += accuracy(logits[train_mask], labels[train_mask])
+    epoch_loss = loss.detach().item()
+    epoch_train_acc = accuracy(logits[train_mask], labels[train_mask])
 
     if writer is not None:
         writer, _ = check_tensorboard(model, writer, step=epoch)
@@ -27,13 +25,11 @@ def train_epoch_sparse(model, optimizer, device, graph, epoch,
 def evaluate_network_sparse(model, device, graph, epoch,
                             nfeat, efeat, mask, labels):
     model.eval()
-    epoch_test_loss = 0
-    epoch_test_acc = 0
     with torch.no_grad():
         logits = model.forward(graph, nfeat, efeat)
         loss = model.loss(logits[mask], labels[mask])
-        epoch_test_loss += loss.detach().item()
-        epoch_test_acc += accuracy(logits[mask], labels[mask])
+        epoch_test_loss = loss.detach().item()
+        epoch_test_acc = accuracy(logits[mask], labels[mask])
 
     return epoch_test_loss, epoch_test_acc
 
