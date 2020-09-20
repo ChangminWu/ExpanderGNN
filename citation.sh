2d550f9ff27be8251a8fae88fe18b00fe3895311
Changmin <47852417+ChangminWu@users.noreply.github.com>
2020-09-20 09:49:57 +0200

    citations modified

--- citation.sh
+++ citation.sh
@@ -1,10 +1,10 @@
 use_gpu="True"
 script=main_citation_node_classification.py
 datasets=("CORA" "CITESEER" "PUBMED")
-actives=('relu' 'prelu' 'linear' 'brelu' 'brelu-intercept' 'conv' 'rrelu' 'elu' 'sigmoid' 'tanh' 'lelu' 'softplus')
-models=('GCN' 'GIN' 'MLP' 'GraphSage' 'GatedGCN' 'GAT' 'PNA')
+actives=('relu' 'prelu' 'brelu' 'rrelu' 'linear' 'softshrink' 'tanh' 'softplus' 'selu' 'lelu')
+models=('GCN' 'GIN' 'MLP' 'GraphSage' 'GatedGCN' 'PNA')
 densities=( 0.1 0.5 0.9 )
-savedir="results/citation-runs/"
+savedir="results/citation-runs-new/"
 
 for i in "${datasets[@]}"
 do
@@ -13,13 +13,18 @@ do
     config_file=configs/citation_node_classification/${j}_citation_100k.json
     for d in "${densities[@]}"
     do
-      python $script --dataset ${i} --out_dir ${savedir} --experiment "expander-density-${d}" --model ${j} --density ${d} --linear_type "expander" --config ${config_file} --mlp_layers 1 --use_gpu $use_gpu
+      python $script --dataset ${i} --out_dir ${savedir} --experiment "expander-density-${d}" --model ${j} --density ${d} --linear_type "expander" --config ${config_file} --mlp_layers 1 --use_gpu $use_gpu --batch_norm False
     done
-    for a in "${actives[@]}"
-    do
-      python $script --dataset ${i} --out_dir ${savedir} --experiment "activations-${a}" --model "Activation${j}" --activation ${a} --config ${config_file} --mlp_layers 1 --use_gpu $use_gpu
-    done
-    python $script --dataset ${i} --out_dir ${savedir} --experiment "simple" --model "Simple${j}" --config ${config_file} --mlp_layers 1
-    python $script --dataset ${i} --out_dir ${savedir} --experiment "regular" --model ${j} --linear_type "regular" --density 1.0 --config ${config_file} --mlp_layers 1 --use_gpu $use_gpu
+
+    if [ "$j" != "MLP" ]
+    then
+      for a in "${actives[@]}"
+      do
+        python $script --dataset ${i} --out_dir ${savedir} --experiment "activations-${a}" --model "Activation${j}" --activation ${a} --config ${config_file} --mlp_layers 1 --use_gpu $use_gpu --batch_norm False --init_lr 0.2
+      done
+      python $script --dataset ${i} --out_dir ${savedir} --experiment "simple" --model "Simple${j}" --config ${config_file} --mlp_layers 1 --use_gpu $use_gpu --batch_norm False --init_lr 0.2
+    fi
+
+    python $script --dataset ${i} --out_dir ${savedir} --experiment "regular" --model ${j} --linear_type "regular" --density 1.0 --config ${config_file} --mlp_layers 1 --use_gpu $use_gpu --batch_norm False
   done
 done
