6f3d196cf39f7012bbb607e979dabde7914c46cd
Changmin <47852417+ChangminWu@users.noreply.github.com>
2020-09-20 22:43:43 +0200

    citations modified -- PNA node encoder added

--- citation.sh
+++ citation.sh
@@ -4,27 +4,27 @@ datasets=("CORA" "CITESEER" "PUBMED")
 actives=('relu' 'prelu' 'brelu' 'rrelu' 'linear' 'softshrink' 'tanh' 'softplus' 'selu' 'lelu')
 models=('PNA') #'GCN' 'GIN' 'MLP' 'GraphSage'
 densities=( 0.1 0.5 0.9 )
-savedir="results/citation-runs-new-gatedgcn/"
+savedir="results/citation-runs-new-PNA/"
 
 for i in "${datasets[@]}"
 do
   for j in "${models[@]}"
   do
     config_file=configs/citation_node_classification/${j}_citation_100k.json
-#    for d in "${densities[@]}"
-#    do
-#      python $script --dataset ${i} --out_dir ${savedir} --experiment "expander-density-${d}" --model ${j} --density ${d} --linear_type "expander" --config ${config_file} --mlp_layers 1 --use_gpu $use_gpu --batch_norm False
-#    done
+    for d in "${densities[@]}"
+    do
+      python $script --dataset ${i} --out_dir ${savedir} --experiment "expander-density-${d}" --model ${j} --density ${d} --linear_type "expander" --config ${config_file} --mlp_layers 1 --use_gpu $use_gpu --batch_norm False
+    done
 
     if [ "$j" != "MLP" ]
     then
-#      for a in "${actives[@]}"
-#      do
-#        python $script --dataset ${i} --out_dir ${savedir} --experiment "activations-${a}" --model "Activation${j}" --activation ${a} --config ${config_file} --mlp_layers 1 --use_gpu $use_gpu --batch_norm False --init_lr 0.2
-#      done
+      for a in "${actives[@]}"
+      do
+        python $script --dataset ${i} --out_dir ${savedir} --experiment "activations-${a}" --model "Activation${j}" --activation ${a} --config ${config_file} --mlp_layers 1 --use_gpu $use_gpu --batch_norm False --init_lr 0.2
+      done
       python $script --dataset ${i} --out_dir ${savedir} --experiment "simple" --model "Simple${j}" --config ${config_file} --mlp_layers 1 --use_gpu $use_gpu --batch_norm False --init_lr 0.2
     fi
 
-#    python $script --dataset ${i} --out_dir ${savedir} --experiment "regular" --model ${j} --linear_type "regular" --density 1.0 --config ${config_file} --mlp_layers 1 --use_gpu $use_gpu --batch_norm False
+    python $script --dataset ${i} --out_dir ${savedir} --experiment "regular" --model ${j} --linear_type "regular" --density 1.0 --config ${config_file} --mlp_layers 1 --use_gpu $use_gpu --batch_norm False
   done
 done
