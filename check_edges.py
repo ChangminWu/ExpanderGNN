ec5a6385dfdea910603be3a76113a1071eb1a579
Changmin <47852417+ChangminWu@users.noreply.github.com>
2020-10-01 09:18:59 +0200

    dataset statistics

--- check_edges.py
+++ check_edges.py
@@ -5,7 +5,7 @@ import argparse
 def count_edges(dataset_name):
     dataset = LoadData(dataset_name)
     num_nodes = [dataset.all[i][0].number_of_nodes() for i in range(len(dataset.all))]
-    num_edges = [dataset.all[i][0].number_of_edges() for i in range(len(dataset.all))]
+    num_edges = [dataset.all[i][0].number_of_edges()/2 for i in range(len(dataset.all))]
     print("#graphs: {}\n#nodes: {}\n#edges: {}".format(len(num_nodes), np.mean(num_nodes), np.mean(num_edges)))
 
 
