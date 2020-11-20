0a84700b7c9dcf983e6f78d63135e4f1755ccf67
Changmin <47852417+ChangminWu@users.noreply.github.com>
2020-11-20 09:43:40 +0100

    molhiv

--- scripts/molhiv.sh
+++ scripts/molhiv.sh
@@ -5,6 +5,7 @@ models=('GCN' 'GIN' 'MLP')
 activs=('tanh' 'relu' 'prelu')
 savedir="results/molhiv/"
 
+densities=(0.01 0.1 0.5 0.9)
 for j in "${models[@]}"
 do
   for i in "${datasets[@]}"
@@ -32,18 +33,47 @@ do
   done
 done
 
+savedir="results/molhiv-hiddims/"
+hiddims=(10 40 70 120 150 200)
+for j in "${models[@]}"
+do
+  for i in "${datasets[@]}"
+  do
+    config_file=configs/tu_graph_classification/${j}_DD_100k.json
+    for h in "${hiddims[@]}"
+    do
+      python $script --dataset "$i" --out_dir $savedir --experiment "regular-dim-${h}" --model "$j" --density 1.0 --linear_type "regular" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --hidden_dim "$h" --out_dim "$h" --dropout 0.5 --init_lr 0.001
+    done
+  done
+done
 
-#densities=(0.01 0.02 0.05 0.08 0.1 0.5 0.9)
-#for j in "${models[@]}"
-#do
-#  for i in "${datasets[@]}"
-#  do
-#    config_file=configs/tu_graph_classification/${j}_DD_100k.json
-#    for d in "${densities[@]}"
-#    do
-#      python $script --dataset "$i" --out_dir $savedir --experiment "random-density-${d}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --sampler "random"
-#      python $script --dataset "$i" --out_dir $savedir --experiment "rotate-density-${d}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --sampler "rotate"
-#      python $script --dataset "$i" --out_dir $savedir --experiment "expander-density-${d}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --sampler "regular"
-#    done
-#  done
-#done
+savedir="results/molhiv-small-models/"
+densities=(0.05 0.1 0.5 0.9)
+for j in "${models[@]}"
+do
+  for i in "${datasets[@]}"
+  do
+    config_file=configs/tu_graph_classification/${j}_DD_100k.json
+    for d in "${densities[@]}"
+    do
+      python $script --dataset "$i" --out_dir $savedir --experiment "small-model-density-${h}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --hidden_dim 30 --out_dim 30 --dropout 0.5 --init_lr 0.001
+    done
+  done
+done
+
+savedir="results/molhiv-samplers/"
+densities=(0.01 0.02 0.05 0.08 0.1 0.5 0.9)
+for j in "${models[@]}"
+do
+  for i in "${datasets[@]}"
+  do
+    config_file=configs/tu_graph_classification/${j}_DD_100k.json
+    for d in "${densities[@]}"
+    do
+      python $script --dataset "$i" --out_dir $savedir --experiment "random-density-${d}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --sampler "random" --hidden_dim 300 --out_dim 300 --dropout 0.5 --init_lr 0.001
+      python $script --dataset "$i" --out_dir $savedir --experiment "rotate-density-${d}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --sampler "rotate" --hidden_dim 300 --out_dim 300 --dropout 0.5 --init_lr 0.001
+      python $script --dataset "$i" --out_dir $savedir --experiment "expander-density-${d}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --sampler "regular" --hidden_dim 300 --out_dim 300 --dropout 0.5 --init_lr 0.001
+    done
+  done
+done
+
