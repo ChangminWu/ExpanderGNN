d07022ec42e766f1178987d023994ed320901d96
Changmin <47852417+ChangminWu@users.noreply.github.com>
2020-11-20 08:21:29 +0100

    molhiv

--- layers/expander/expander_layer.py
+++ layers/expander/expander_layer.py
@@ -19,6 +19,7 @@ class LinearLayer(nn.Module):
             self.layer = ExpanderLinear(indim, outdim, bias=self.bias,
                                         **kwargs)
         elif self.linear_type == "regular":
+            print(indim, outdim)
             self.layer = nn.Linear(indim, outdim, bias=self.bias)
             self.reset_parameters()
         else:
