cd6799cdd5adf0d9d5b660cc14f46da9483c75f6
Changmin <47852417+ChangminWu@users.noreply.github.com>
2020-11-20 21:38:14 +0100

    molhiv

--- data/data.py
+++ data/data.py
@@ -4,7 +4,7 @@ from data.citations import CitationsDataset
 def LoadData(DATASET_NAME):
     TU_DATASETS = ["COLLAB", "ENZYMES", "DD", "PROTEINS_full", "IMDB-BINARY",
                    "IMDB-MULTI", "REDDIT-BINARY", "REDDIT-MULTI-5K",
-                   "REDDIT-MULTI-12K", "PTC-MR"]
+                   "REDDIT-MULTI-12K", "PTC-MR", "MUTAG"]
     if DATASET_NAME in TU_DATASETS:
         return TUsDataset(DATASET_NAME)
 
