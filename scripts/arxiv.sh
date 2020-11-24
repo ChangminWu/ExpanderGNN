c54e5ba70df6426e345b6f288137623be66d2fd0
Changmin <47852417+ChangminWu@users.noreply.github.com>
2020-11-24 03:19:42 +0100

    new random samplers

--- scripts/arxiv.sh
+++ scripts/arxiv.sh
@@ -1,7 +1,7 @@
 use_gpu=True
 models=('GCN')
 
-#densities=(0.01 0.02 0.05 0.08 0.1 0.5 0.9)
+densities=(0.005 0.01 0.02 0.05 0.08 0.1 0.5 0.9)
 #
 #script=main_arxiv_node_classification.py
 #savedir="results/arxiv-samplers/"
@@ -20,22 +20,22 @@ models=('GCN')
 #  done
 #done
 #
-#script=main_citation_node_classification.py
-#savedir="results/citation-samplers/"
-#datasets=("CORA" "CITESEER" "PUBMED")
-#for j in "${models[@]}"
-#do
-#  for i in "${datasets[@]}"
-#  do
-#    config_file=configs/citation_node_classification/${j}_citation_100k.json
-#    for d in "${densities[@]}"
-#    do
-#      python $script --dataset "$i" --out_dir $savedir --experiment "random-density-${d}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --sampler "random" --batch_norm False --L 2
+script=main_citation_node_classification.py
+savedir="results/citation-samplers/"
+datasets=("PUBMED" "CORA" "CITESEER")
+for j in "${models[@]}"
+do
+  for i in "${datasets[@]}"
+  do
+    config_file=configs/citation_node_classification/${j}_citation_100k.json
+    for d in "${densities[@]}"
+    do
+      python $script --dataset "$i" --out_dir $savedir --experiment "random-density-${d}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --sampler "random" --batch_norm False --L 2
 #      python $script --dataset "$i" --out_dir $savedir --experiment "rotate-density-${d}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --sampler "rotate" --batch_norm False --L 2
-#      python $script --dataset "$i" --out_dir $savedir --experiment "expander-density-${d}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --sampler "regular" --batch_norm False --L 2
-#    done
-#  done
-#done
+      python $script --dataset "$i" --out_dir $savedir --experiment "expander-density-${d}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --sampler "regular" --batch_norm False --L 2
+    done
+  done
+done
 
 #hiddims=(2 4 8 16 32 64 128 192)
 #
@@ -54,7 +54,7 @@ models=('GCN')
 #  done
 #done
 #
-hiddims=(4 8 16 32 64 128 256 384)
+#hiddims=(4 8 16 32 64 128 256 384)
 #
 #script=main_arxiv_node_classification.py
 #savedir="results/arxiv-match-hiddims/"
@@ -71,20 +71,20 @@ hiddims=(4 8 16 32 64 128 256 384)
 #  done
 #done
 
-script=main_citation_node_classification.py
-savedir="results/citation-match-hiddims/"
-datasets=("PUBMED" "CORA" "CITESEER")
-for j in "${models[@]}"
-do
-  for i in "${datasets[@]}"
-  do
-    config_file=configs/citation_node_classification/${j}_citation_100k.json
-    for h in "${hiddims[@]}"
-    do
-      python $script --dataset "$i" --out_dir $savedir --experiment "random-dim-${h}" --model "$j" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --batch_norm False --L 2 --hidden_dim "$h" --out_dim "$h" --sampler random
-    done
-  done
-done
+#script=main_citation_node_classification.py
+#savedir="results/citation-match-hiddims/"
+#datasets=("PUBMED" "CORA" "CITESEER")
+#for j in "${models[@]}"
+#do
+#  for i in "${datasets[@]}"
+#  do
+#    config_file=configs/citation_node_classification/${j}_citation_100k.json
+#    for h in "${hiddims[@]}"
+#    do
+#      python $script --dataset "$i" --out_dir $savedir --experiment "random-dim-${h}" --model "$j" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --batch_norm False --L 2 --hidden_dim "$h" --out_dim "$h" --sampler random
+#    done
+#  done
+#done
 
 
 #densities=(0.05 0.1 0.5 0.9)
