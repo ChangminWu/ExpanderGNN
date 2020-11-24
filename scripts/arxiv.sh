909d7292c7ceaa76c2584e344a145b99bc3fedb4
Changmin <47852417+ChangminWu@users.noreply.github.com>
2020-11-24 01:17:33 +0100

    fix density

--- scripts/arxiv.sh
+++ scripts/arxiv.sh
@@ -37,26 +37,11 @@ models=('GCN')
 #  done
 #done
 
-hiddims=(8 16 32 64 128 256 512 768)
+hiddims=(2 4 8 16 32 64 128 192)
 
-#script=main_arxiv_node_classification.py
-#savedir="results/arxiv-hiddims/"
-#datasets=('ogbn-arxiv')
-#for j in "${models[@]}"
-#do
-#  for i in "${datasets[@]}"
-#  do
-#    config_file=configs/citation_node_classification/${j}_citation_100k.json
-#    for h in "${hiddims[@]}"
-#    do
-#      python $script --dataset ogbn-arxiv --out_dir $savedir --experiment "regular-dim-${h}" --model "$j" --density 1.0 --linear_type "regular" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --dropout 0.5 --epochs 1000 --init_lr 0.005
-#    done
-#  done
-#done
-
-script=main_citation_node_classification.py
-savedir="results/citation-match-hiddims/"
-datasets=("PUBMED" "CORA" "CITESEER")
+script=main_arxiv_node_classification.py
+savedir="results/arxiv-hiddims/"
+datasets=('ogbn-arxiv')
 for j in "${models[@]}"
 do
   for i in "${datasets[@]}"
@@ -64,10 +49,42 @@ do
     config_file=configs/citation_node_classification/${j}_citation_100k.json
     for h in "${hiddims[@]}"
     do
-      python $script --dataset "$i" --out_dir $savedir --experiment "expander-dim-${h}" --model "$j" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --batch_norm False --L 2 --hidden_dim "$h" --out_dim "$h"
+      python $script --dataset ogbn-arxiv --out_dir $savedir --experiment "regular-dim-${h}" --model "$j" --density 1.0 --linear_type "regular" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --dropout 0.5 --epochs 1000 --init_lr 0.005
     done
   done
 done
+
+hiddims=(4 8 16 32 64 128 256 384)
+
+script=main_arxiv_node_classification.py
+savedir="results/arxiv-match-hiddims/"
+datasets=('ogbn-arxiv')
+for j in "${models[@]}"
+do
+  for i in "${datasets[@]}"
+  do
+    config_file=configs/citation_node_classification/${j}_citation_100k.json
+    for h in "${hiddims[@]}"
+    do
+      python $script --dataset ogbn-arxiv --out_dir $savedir --experiment "expander-dim-${h}" --model "$j" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --dropout 0.5 --epochs 1000 --init_lr 0.005
+    done
+  done
+done
+
+#script=main_citation_node_classification.py
+#savedir="results/citation-match-hiddims/"
+#datasets=("PUBMED" "CORA" "CITESEER")
+#for j in "${models[@]}"
+#do
+#  for i in "${datasets[@]}"
+#  do
+#    config_file=configs/citation_node_classification/${j}_citation_100k.json
+#    for h in "${hiddims[@]}"
+#    do
+#      python $script --dataset "$i" --out_dir $savedir --experiment "expander-dim-${h}" --model "$j" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --batch_norm False --L 2 --hidden_dim "$h" --out_dim "$h"
+#    done
+#  done
+#done
 
 
 #densities=(0.05 0.1 0.5 0.9)
