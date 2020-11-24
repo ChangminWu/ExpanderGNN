5b21979820869eabae98138ce2fc411c05899051
Changmin <47852417+ChangminWu@users.noreply.github.com>
2020-11-24 12:00:48 +0100

    new random samplers

--- scripts/arxiv.sh
+++ scripts/arxiv.sh
@@ -1,7 +1,7 @@
 use_gpu=True
 models=('GCN')
 
-densities=(0.005 0.01 0.02 0.05 0.08 0.1 0.5 0.9)
+#densities=(0.005 0.01 0.02 0.05 0.08 0.1 0.5 0.9)
 #
 #script=main_arxiv_node_classification.py
 #savedir="results/arxiv-samplers/"
@@ -20,56 +20,56 @@ densities=(0.005 0.01 0.02 0.05 0.08 0.1 0.5 0.9)
 #  done
 #done
 #
-script=main_citation_node_classification.py
-savedir="results/citation-samplers/"
-datasets=("PUBMED" "CORA" "CITESEER")
+#script=main_citation_node_classification.py
+#savedir="results/citation-samplers/"
+#datasets=("PUBMED" "CORA" "CITESEER")
+#for j in "${models[@]}"
+#do
+#  for i in "${datasets[@]}"
+#  do
+#    config_file=configs/citation_node_classification/${j}_citation_100k.json
+#    for d in "${densities[@]}"
+#    do
+#      python $script --dataset "$i" --out_dir $savedir --experiment "random-density-${d}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --sampler "random" --batch_norm False --L 2
+##      python $script --dataset "$i" --out_dir $savedir --experiment "rotate-density-${d}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --sampler "rotate" --batch_norm False --L 2
+#      python $script --dataset "$i" --out_dir $savedir --experiment "expander-density-${d}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --sampler "regular" --batch_norm False --L 2
+#    done
+#  done
+#done
+
+hiddims=(2 4 8 16 32 64 128 192)
+
+script=main_arxiv_node_classification.py
+savedir="results/arxiv-hiddims/"
+datasets=('ogbn-arxiv')
 for j in "${models[@]}"
 do
   for i in "${datasets[@]}"
   do
     config_file=configs/citation_node_classification/${j}_citation_100k.json
-    for d in "${densities[@]}"
+    for h in "${hiddims[@]}"
     do
-      python $script --dataset "$i" --out_dir $savedir --experiment "random-density-${d}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --sampler "random" --batch_norm False --L 2
-#      python $script --dataset "$i" --out_dir $savedir --experiment "rotate-density-${d}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --sampler "rotate" --batch_norm False --L 2
-      python $script --dataset "$i" --out_dir $savedir --experiment "expander-density-${d}" --model "$j" --density "$d" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --sampler "regular" --batch_norm False --L 2
+      python $script --dataset ogbn-arxiv --out_dir $savedir --experiment "regular-dim-${h}" --model "$j" --density 1.0 --linear_type "regular" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --dropout 0.5 --epochs 1000 --init_lr 0.005
     done
   done
 done
 
-#hiddims=(2 4 8 16 32 64 128 192)
-#
-#script=main_arxiv_node_classification.py
-#savedir="results/arxiv-hiddims/"
-#datasets=('ogbn-arxiv')
-#for j in "${models[@]}"
-#do
-#  for i in "${datasets[@]}"
-#  do
-#    config_file=configs/citation_node_classification/${j}_citation_100k.json
-#    for h in "${hiddims[@]}"
-#    do
-#      python $script --dataset ogbn-arxiv --out_dir $savedir --experiment "regular-dim-${h}" --model "$j" --density 1.0 --linear_type "regular" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --dropout 0.5 --epochs 1000 --init_lr 0.005
-#    done
-#  done
-#done
-#
-#hiddims=(4 8 16 32 64 128 256 384)
-#
-#script=main_arxiv_node_classification.py
-#savedir="results/arxiv-match-hiddims/"
-#datasets=('ogbn-arxiv')
-#for j in "${models[@]}"
-#do
-#  for i in "${datasets[@]}"
-#  do
-#    config_file=configs/citation_node_classification/${j}_citation_100k.json
-#    for h in "${hiddims[@]}"
-#    do
-#      python $script --dataset ogbn-arxiv --out_dir $savedir --experiment "expander-dim-${h}" --model "$j" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --dropout 0.5 --epochs 1000 --init_lr 0.005
-#    done
-#  done
-#done
+hiddims=(4 8 16 32 64 128 256 384)
+
+script=main_arxiv_node_classification.py
+savedir="results/arxiv-match-hiddims/"
+datasets=('ogbn-arxiv')
+for j in "${models[@]}"
+do
+  for i in "${datasets[@]}"
+  do
+    config_file=configs/citation_node_classification/${j}_citation_100k.json
+    for h in "${hiddims[@]}"
+    do
+      python $script --dataset ogbn-arxiv --out_dir $savedir --experiment "expander-dim-${h}" --model "$j" --linear_type "expander" --config "$config_file" --mlp_layers 1 --use_gpu $use_gpu --dropout 0.5 --epochs 1000 --init_lr 0.005
+    done
+  done
+done
 
 #script=main_citation_node_classification.py
 #savedir="results/citation-match-hiddims/"
