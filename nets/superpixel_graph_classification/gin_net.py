868a2405f0136b8fec5b9f0de634c1df43d5f859
Changmin <47852417+ChangminWu@users.noreply.github.com>
2021-01-27 13:27:02 +0100

    typo

--- nets/superpixel_graph_classification/gin_net.py
+++ nets/superpixel_graph_classification/gin_net.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 from dgl.nn.pytorch.glob import SumPooling, AvgPooling, MaxPooling
--- nets/superpixel_graph_classification/graphsage_net.py
+++ nets/superpixel_graph_classification/graphsage_net.py
@@ -1,4 +1,5 @@
-import torch.nn as nn 
+import torch
+import torch.nn as nn
 
 import dgl
 
