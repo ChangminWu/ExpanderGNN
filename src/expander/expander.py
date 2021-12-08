870b98621754ec9ac493bb03eef7e0138fa918f8
ChangminWu <changmin.wu@polytechnique.edu>
2021-12-08 18:35:45 +0100

    update

--- src/expander/expander.py
+++ src/expander/expander.py
@@ -1,7 +1,72 @@
 import torch
-import torch.nn as nn   
+from torch import Tensor
+import torch.nn as nn
+from torch.nn.parameter import Parameter
+from torch_sparse import SparseTensor
+
+import math
+from typing import Optional
+
 
 class ExpanderLinear(nn.Module):
-    def __init__(self, indim, outdim, bias=True, edge_index=None):
-        self.indim, self.outdim = indim, outdim
+    __constants__ = ["indim", "outdim"]
+    indim: int
+    outdim: int
+    weight: SparseTensor
+
+    def __init__(self, indim: int, outdim: int, bias: bool=True, edge_index: Optional[Tensor]=None, weight_initializer: Optional[str]=None) -> None:
+        super().__init__()
+
+        self.indim, self.outdim, self.edge_index = indim, outdim, self.edge_index
+        row, col = self.edge_index
+
+        if bias:
+            self.bias = Parameter(torch.Tensor(outdim))
+        else:
+            self.register_parameter("bias", None)
+
+        self.nnz_weight = Parameter(torch.Tensor(len(row)))
+        self.weight = SparseTensor(row=row, col=col, sparse_sizes=(outdim, indim), value=self.nnz_weight)
+        
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        if self.bias is not None:
+            nn.init.zeros_(self.bias)
+
+        if self.weight_initializer == "glorot":
+            stdv = math.sqrt(6.0 / (self.indim + self.outdim))
+            self.nnz_weight.data.uniform_(-stdv, stdv)
+        elif self.weight_initializer == "glorot-full":
+            stdv = math.sqrt(6.0 / self.weight.nnz())
+            self.nnz_weight.data.uniform_(-stdv, stdv)
+        elif self.weight_initializer == "uniform":
+            bound = 1.0 / math.sqrt(self.indim)
+            self.nnz_weight.data.uniform(-bound, bound)
+        elif self.weight_initializer == None:
+            bound = 1.0 / math.sqrt(self.weight.nnz())
+            self.nnz_weight.data.uniform(-bound, bound)
+        self.weight.set_value_(self.nnz_weight)
+
+    def forward(self, x: Tensor) -> Tensor:
+        if self.bias is not None:
+            return self.weight.matmul(x.t()).t() + self.bias
+        else:
+            return self.weight.matmul(x.t()).t()
+            
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}({self.indim}, '
+                f'{self.outdim}, bias={self.bias is not None})')
+
+
+        
+
+
+
+
+
+        
+
+
+
 
--- src/expander/samplers.py
+++ src/expander/samplers.py
@@ -1,22 +1,41 @@
 import torch
+from torch import Tensor
 import numpy as np
 
 
-def regular_sampler(outdim, indim, density):
+def sampler(indim: int, outdim: int, density: float, sample_method: str="prabhu") -> Tensor:
     row_idx = []
     col_idx = []
-    if outdim < indim:
-        for i in range(outdim):
-            num_connections = max(1, int(indim*density))
-            x = np.random.permutation(indim)
-            row_idx.extend([i]*num_connections)
-            col_idx.extend(list(x[:num_connections]))
-    else:
-        for i in range(indim):
-            num_connections = max(1, int(outdim*density))
-            x = np.random.permutation(outdim)
-            row_idx.extend(list(x[:num_connections]))
-            col_idx.extend([i]*num_connections)
+    
+    if sample_method.lower() == "prabhu":
+        if outdim < indim:
+            for i in range(outdim):
+                num_connections = max(1, int(indim*density))
+                x = np.random.permutation(indim)
+                row_idx.extend([i]*num_connections)
+                col_idx.extend(list(x[:num_connections]))
+        else:
+            for i in range(indim):
+                num_connections = max(1, int(outdim*density))
+                x = np.random.permutation(outdim)
+                row_idx.extend(list(x[:num_connections]))
+                col_idx.extend([i]*num_connections)
+    
+    elif sample_method.lower() == "random":
+        num_connections = int(indim*outdim*density)
+        while True:
+            rind = torch.randint(outdim, size=(int(num_connections*1.5),)).reshape(-1, 1)
+            cind = torch.randint(indim, size=(int(num_connections*1.5),)).reshape(-1, 1)
+            t_ = torch.cat([rind, cind], dim=1)
+            t_ = set([(int(x[0]), int(x[1])) for x in t_])
+            if len(t_) >= num_connections:
+                row_idx = [x[0] for x in t_]
+                col_idx = [x[1] for x in t_]
+                row_idx = row_idx[:num_connections]
+                col_idx = col_idx[:num_connections]
+                break
+        
     row = torch.LongTensor(row_idx)
     col = torch.LongTensor(col_idx)
-    return row, col
+    edge_index = torch.stack([row, col], dim=0).long()
+    return edge_index
