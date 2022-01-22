155f748b704f2aae208142bb4052d4321be61907
Changmin <47852417+ChangminWu@users.noreply.github.com>
2022-01-23 00:54:25 +0100

    update

--- src/layers/convs.py
+++ src/layers/convs.py
@@ -68,43 +68,43 @@ class ActivationGCNConv(GCNConv):
         super().__init__(indim, outdim, improved, cached, add_self_loops, normalize, False, **kwargs)
         self.lin = torch.nn.Identity()
 
-    def forward(self, x: Tensor, edge_index: Adj, edge_weight: OptTensor = None) -> Tensor:
-            """"""
+    # def forward(self, x: Tensor, edge_index: Adj, edge_weight: OptTensor = None) -> Tensor:
+    #         """"""
 
-            if self.normalize:
-                if isinstance(edge_index, Tensor):
-                    cache = self._cached_edge_index
-                    if cache is None:
-                        edge_index, edge_weight = gcn_norm(  # yapf: disable
-                            edge_index, edge_weight, x.size(self.node_dim),
-                            self.improved, self.add_self_loops)
-                        if self.cached:
-                            self._cached_edge_index = (edge_index, edge_weight)
-                    else:
-                        edge_index, edge_weight = cache[0], cache[1]
+    #         if self.normalize:
+    #             if isinstance(edge_index, Tensor):
+    #                 cache = self._cached_edge_index
+    #                 if cache is None:
+    #                     edge_index, edge_weight = gcn_norm(  # yapf: disable
+    #                         edge_index, edge_weight, x.size(self.node_dim),
+    #                         self.improved, self.add_self_loops)
+    #                     if self.cached:
+    #                         self._cached_edge_index = (edge_index, edge_weight)
+    #                 else:
+    #                     edge_index, edge_weight = cache[0], cache[1]
 
-                elif isinstance(edge_index, SparseTensor):
-                    cache = self._cached_adj_t
-                    if cache is None:
-                        edge_index = gcn_norm(  # yapf: disable
-                            edge_index, edge_weight, x.size(self.node_dim),
-                            self.improved, self.add_self_loops)
-                        if self.cached:
-                            self._cached_adj_t = edge_index
-                    else:
-                        edge_index = cache
+    #             elif isinstance(edge_index, SparseTensor):
+    #                 cache = self._cached_adj_t
+    #                 if cache is None:
+    #                     edge_index = gcn_norm(  # yapf: disable
+    #                         edge_index, edge_weight, x.size(self.node_dim),
+    #                         self.improved, self.add_self_loops)
+    #                     if self.cached:
+    #                         self._cached_adj_t = edge_index
+    #                 else:
+    #                     edge_index = cache
 
-            print("before iden, ", x)
-            x = self.lin(x)
-            print("after iden, ", x)
-            # propagate_type: (x: Tensor, edge_weight: OptTensor)
-            out = self.propagate(edge_index, x=x, edge_weight=edge_weight,
-                                size=None)
-            print("after prop, ", out)
-            if self.bias is not None:
-                out += self.bias
+    #         print("before iden, ", x)
+    #         x = self.lin(x)
+    #         print("after iden, ", x)
+    #         # propagate_type: (x: Tensor, edge_weight: OptTensor)
+    #         out = self.propagate(edge_index, x=x, edge_weight=edge_weight,
+    #                             size=None)
+    #         print("after prop, ", out)
+    #         if self.bias is not None:
+    #             out += self.bias
 
-            return out
+    #         return out
 
     def reset_parameters(self):
         self._cached_edge_index = None
