7e54a25efa31d385e9e4b8714286f6d670a2081f
Changmin <47852417+ChangminWu@users.noreply.github.com>
2022-02-04 10:38:06 +0100

    update

--- src/expander/expander.py
+++ src/expander/expander.py
@@ -27,7 +27,7 @@ class ExpanderLinear(nn.Module):
             self.register_parameter("bias", None)
 
         self.nnz_weight = Parameter(torch.cuda.FloatTensor(len(row)))
-        self.weight = SparseTensor(row=row.to(torch.device('gpu')), col=col.to(torch.device('gpu')), sparse_sizes=(outdim, indim), value=self.nnz_weight)
+        self.weight = SparseTensor(row=row.to(torch.device('cuda')), col=col.to(torch.device('cuda')), sparse_sizes=(outdim, indim), value=self.nnz_weight)
         
         # self.reset_parameters()
 
