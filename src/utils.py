b96feb08f41eba91d571b4935dc23ffb14361151
Changmin <47852417+ChangminWu@users.noreply.github.com>
2022-02-04 07:42:15 +0100

    update

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 import os
-import 
+# import 
 
 from ogb.nodeproppred import Evaluator as OGBNEvaluator
 from ogb.graphproppred import Evaluator as OGBGEvaluator
@@ -50,63 +50,62 @@ class Evaluator(object):
         return metric
 
 
-class LoadDataset(object):
-    def __init__(self, root, name, pre_transform=None, transform=None):
-        self.root = root
-        self.name = name.lower()
-        self.pre_transform = pre_transform
-        self.transform = transform
+# class LoadDataset(object):
+#     def __init__(self, root, name, pre_transform=None, transform=None):
+#         self.root = root
+#         self.name = name.lower()
+#         self.pre_transform = pre_transform
+#         self.transform = transform
 
-    def load(self):
-        if self.name == 'cora':
-            self.dataset = Planetoid(root=os.path.join(self.root, 'datasets', self.name), name='Cora', )
-        elif self.name == 'citeseer':
-            self.dataset = Planetoid(root=os.path.join(self.root, 'datasets', self.name), name='CiteSeer', )
-
-        else:
-            raise NotImplementedError(self.name)
+#     def load(self):
+#         if self.name == 'cora':
+#             self.dataset = Planetoid(root=os.path.join(self.root, 'datasets', self.name), name='Cora', )
+#         elif self.name == 'citeseer':
+#             self.dataset = Planetoid(root=os.path.join(self.root, 'datasets', self.name), name='CiteSeer', )
+#         else:
+#             raise NotImplementedError(self.name)
     
-    def _ogb(self, input_dict, **kwargs):
-        assert 'y_true' in input_dict
-        assert input_dict['y_true'] is not None
-        assert 'y_pred' in input_dict
-        assert input_dict['y_pred'] is not None
-        return self._ogb_evaluator.eval(input_dict)[self._key]
+#     def _ogb(self, input_dict, **kwargs):
+#         assert 'y_true' in input_dict
+#         assert input_dict['y_true'] is not None
+#         assert 'y_pred' in input_dict
+#         assert input_dict['y_pred'] is not None
+#         return self._ogb_evaluator.eval(input_dict)[self._key]
 
 
-def gcn_norm(edge_index, edge_weight=None, num_nodes=None, improved=False,
-             add_self_loops=True, dtype=None):
+# def gcn_norm(edge_index, edge_weight=None, num_nodes=None, improved=False,
+#              add_self_loops=True, dtype=None):
 
-    fill_value = 2. if improved else 1.
+#     fill_value = 2. if improved else 1.
 
-    if isinstance(edge_index, SparseTensor):
-        adj_t = edge_index
-        if not adj_t.has_value():
-            adj_t = adj_t.fill_value(1., dtype=dtype)
-        if add_self_loops:
-            adj_t = fill_diag(adj_t, fill_value)
-        deg = sparsesum(adj_t, dim=1)
-        deg_inv_sqrt = deg.pow_(-0.5)
-        deg_inv_sqrt.masked_fill_(deg_inv_sqrt == float('inf'), 0.)
-        adj_t = mul(adj_t, deg_inv_sqrt.view(-1, 1))
-        adj_t = mul(adj_t, deg_inv_sqrt.view(1, -1))
-        return adj_t
+#     if isinstance(edge_index, SparseTensor):
+#         adj_t = edge_index
+#         if not adj_t.has_value():
+#             adj_t = adj_t.fill_value(1., dtype=dtype)
+#         if add_self_loops:
+#             adj_t = fill_diag(adj_t, fill_value)
+#         deg = sparsesum(adj_t, dim=1)
+#         deg_inv_sqrt = deg.pow_(-0.5)
+#         deg_inv_sqrt.masked_fill_(deg_inv_sqrt == float('inf'), 0.)
+#         adj_t = mul(adj_t, deg_inv_sqrt.view(-1, 1))
+#         adj_t = mul(adj_t, deg_inv_sqrt.view(1, -1))
+#         return adj_t
 
-    else:
-        num_nodes = maybe_num_nodes(edge_index, num_nodes)
+#     else:
+#         num_nodes = maybe_num_nodes(edge_index, num_nodes)
 
-        if edge_weight is None:
-            edge_weight = torch.ones((edge_index.size(1), ), dtype=dtype,
-                                     device=edge_index.device)
+#         if edge_weight is None:
+#             edge_weight = torch.ones((edge_index.size(1), ), dtype=dtype,
+#                                      device=edge_index.device)
 
-        if add_self_loops:
-            edge_index, tmp_edge_weight = add_remaining_self_loops(
-                edge_index, edge_weight, fill_value, num_nodes)
-            assert tmp_edge_weight is not None
-            edge_weight = tmp_edge_weight
+#         if add_self_loops:
+#             edge_index, tmp_edge_weight = add_remaining_self_loops(
+#                 edge_index, edge_weight, fill_value, num_nodes)
+#             assert tmp_edge_weight is not None
+#             edge_weight = tmp_edge_weight
 
-        row, col = edge_index[0], edge_index[1]
-        deg = scatter_add(edge_weight, col, dim=0, dim_size=num_nodes)
-        deg_inv_sqrt = deg.pow_(-0.5)
-        deg_inv_sqrt.masked_fill_(deg_inv_sqrt == float('inf'), 0)
-        return edge_index, deg_inv_sqrt[row] * edge_weight * deg_inv_sqrt[col]
+#         row, col = edge_index[0], edge_index[1]
+#         deg = scatter_add(edge_weight, col, dim=0, dim_size=num_nodes)
+#         deg_inv_sqrt = deg.pow_(-0.5)
+#         deg_inv_sqrt.masked_fill_(deg_inv_sqrt == float('inf'), 0)
+#         return edge_index, deg_inv_sqrt[row] * edge_weight * deg_inv_sqrt[col]
