f10bf301ab0f83d60d044940c63adc0ac5f3e27e
ChangminWu <changmin.wu@polytechnique.edu>
2022-02-16 18:29:30 +0100

    update

--- src/expander/samplers.py
+++ src/expander/samplers.py
@@ -21,28 +21,28 @@ def sampler(indim: int, outdim: int, density: float, sample_method: str="prabhu"
                 x = np.random.permutation(outdim)
                 row_idx.extend(list(x[:num_connections]))
                 col_idx.extend([i]*num_connections)
-    
-    elif sample_method.lower() == "random":
-        num_connections = int(indim*outdim*density)
-        t_ = np.random.permutation(list(itertools.product(range(outdim), range(indim))))
-        row_idx = [x[0] for x in t_[:num_connections]]
-        col_idx = [x[1] for x in t_[:num_connections]]
-        # while True:
-        #     rind = torch.randint(outdim, size=(int(num_connections*1.5),)).reshape(-1, 1)
-        #     cind = torch.randint(indim, size=(int(num_connections*1.5),)).reshape(-1, 1)
-        #     t_ = torch.cat([rind, cind], dim=1)
-        #     t_ = set([(int(x[0]), int(x[1])) for x in t_])
-        #     if len(t_) >= num_connections:
-        #         row_idx = [x[0] for x in t_]
-        #         col_idx = [x[1] for x in t_]
-        #         row_idx = row_idx[:num_connections]
-        #         col_idx = col_idx[:num_connections]
-        #         break
-    
+
     elif sample_method.lower() == "identity":
         assert indim==outdim, "activation-only needs same input and output dimension"
         row_idx = [i for i in range(indim)]
         col_idx = [i for i in range(outdim)]
+    
+    elif sample_method.lower() == "random":
+        num_connections = max(1, int(indim*outdim*density))
+        t_ = np.random.permutation(list(itertools.product(range(outdim), range(indim))))
+        row_idx = [x[0] for x in t_[:num_connections]]
+        col_idx = [x[1] for x in t_[:num_connections]]
+    
+    elif sample_method.lower() == "regular-rotate":
+        num_connections = max(1, int(indim*density))
+        for i in range(outdim):
+            row_idx.extend([i]*num_connections)
+            col_idx.extend(range((i%indim, i+num_connections%indim)))
+
+    elif sample_method.lower() == "fibonnaci-rotate":
+        
+
+
 
     row = torch.LongTensor(row_idx)
     col = torch.LongTensor(col_idx)
